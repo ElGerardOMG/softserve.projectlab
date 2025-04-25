08dc256b30d53b918160a0456e4d5decd282cb7c
ElGerardOMG <gerardomg119@gmail.com>
2025-04-24 22:58:20 -0600

    Rename de algunos namespaces

--- API/Models/CartDetail.cs
+++ API/Models/CartDetail.cs
@@ -6,7 +6,7 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Models.Entities;
+namespace API.Models;
 
 [Keyless]
 [Table("cart_detail")]
--- API/Models/User.cs
+++ API/Models/User.cs
@@ -7,7 +7,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
-namespace API.Models.Entities;
+namespace API.Models;
 
 [Table("user")]
 public class User : IdentityUser<int>
